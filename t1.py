bf193a62dcff52e58b032e1e90dc4118d3802e54
Arnon de Botton <arnondebotton@MacBookAir.lan>
2025-08-30 17:37:27 +0300

    Add Streamlit meal tracker app and remove old files

--- t1.py
+++
@@ -1,8 +0,0 @@
-def main():
-  number = int(input("Number: "))
-  if 0 <= number <= 9:
-    print('Number of donuts:' , number)
-  elif 10 <= number:
-    print ('Number of donuts: many')
-if __name__ == '__main__':
-  main()
--- word_frequency.py
+++
@@ -1,8 +0,0 @@
-def word_frequency_counter():
-  words = sentence.split()
-# mydict = dict(Alon=1, Uri=2, Yarivi=3)
-#  print(mydict)
-  dictionary = dict(for item in words(", "))
-  print(dictionary)
-sentence = str(input('Please type a sentence: '))
-word_frequency_counter()
